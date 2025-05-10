5a397da292d96a3dedf739fb75489a465f03158e
sage <antoshkakorol301@gmail.com>
2025-05-09 23:57:44 -0500

    final

--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970094f4aa2cdfd2d9c08f99422bcc3cc52752f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251a72edbb9ef3d515076cb5608ec711d162a29a")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
